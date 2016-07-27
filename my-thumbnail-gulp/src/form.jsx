13f956caebdf0d67a1c7e8126bb50881cac975b1
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-07-27 14:15:21 -0600

    fixing requires for react jsx files

--- my-thumbnail-gulp/src/form.jsx
+++ my-thumbnail-gulp/src/form.jsx
@@ -1,8 +1,10 @@
-var Badge = require('badge');
-
-//render function, sometime later
-render: function() {
-  return <div>
-    <Badge text={this.prop.text} number={this.prop.number} />
-  </div>
-}
+// var Badge = require('badge');
+// var Autocompleter = require('autocompleter');
+//
+// //render function, sometime later
+// render: function() {
+//   return <div>
+//     <Autocompleter list={this.props.list} />
+//     <Badge text={this.prop.text} number={this.prop.number} />
+//   </div>
+// }
